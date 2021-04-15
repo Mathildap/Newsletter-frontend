5c7d165af4783042b07626a65755c27768f28556
Mathilda Pettersson <mathildapetterssonn@hotmail.com>
2021-04-15 15:23:18 +0200

    post with newuser to server

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ const main = document.querySelector('main');
 
 printStartPage();
 
+document.getElementById('newUserBtn').addEventListener("click", () => {
+    newUser();
+});
+
 // EVT-LISTENER ON STARTPAGE
 document.getElementById('startPageLogInBtn').addEventListener("click", () => {
     printLogInPage();
@@ -18,13 +22,12 @@ function printStartPage() {
     startPageContainer.insertAdjacentHTML("beforeend", `  
     <h2>Skapa konto</h2>
     <article id="newUserContainer" class="new-user-container">
-        <form>
+        <div>
             <input type="text" placeholder="Användarnamn" id="newUserName"><br>
             <input type="password" placeholder="Lösenord" id="newPassWord">
             <p class="mail-checkbox-p">Prenumerera på veckobrev?<input type="checkbox" id="mailCheckbox" class="mail-checkbox"></p>
-            <button type="submit" id="newUserBtn" class="new-user-btn">Skapa konto</button>
-        </form>
-
+        <button type="submit" id="newUserBtn" class="new-user-btn">Skapa konto</button>
+        </div>
         <p id="errorNewUserMsg" class="error-new-user-msg"></p>
 
     </article>
@@ -34,6 +37,32 @@ function printStartPage() {
         <button id="startPageLogInBtn">Logga in</button>
     </article>
     `)
+};
+
+//SKAPA EN INLOGGAD-SIDA
+
+function newUser() {
+    let uName = document.getElementById('newUserName').value;
+    let pWord = document.getElementById('newPassWord').value;
+    let mailCheckbox = document.getElementById('mailCheckbox');
+    let subscribe;
+    
+    if (mailCheckbox.checked) {
+        console.log("Prenumerera");
+        subscribe = true;
+    } else {
+        console.log("Ej prenumerera");
+        subscribe = false;
+    };
+
+    newUser = {userName: uName, passWord: pWord, newsLetter: subscribe};
+    console.log(newUser);
+
+    fetch('http://localhost:3000/users/', {method: "post", headers: {"Content-type": "application/json"}, body: JSON.stringify(newUser)})
+    .then(resp => resp.json())
+    .then(data => {
+        console.log(data);
+    });
 };
 
 // FUNCTION - PRINT LOGIN PAGE
